6c59ebc40d913ac1ef71977a9704aa889638714c
Rocco Bruyn <rocco.bruyn@gmail.com>
2017-02-09 01:00:04 +0100

    Add HTTP Client

---
+++ src/api/http-client/error.js
@@ -0,0 +1,6 @@
+// invalid response
+export const INVALID_RESPONSE = {
+  code       : '101',
+  message    : 'Invalid response',
+  description: 'The HTTP response format is invalid and cannot be parsed'
+};
