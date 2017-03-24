addd6c79a47f07a983746ff00ecb5522d9a9e507
Rocco Bruyn <rocco.bruyn@gmail.com>
2017-03-24 16:20:53 +0100

    Add more tests

--- test/fixture.sql
+++ test/fixture.sql
@@ -40,3 +40,46 @@ INSERT INTO public.diver
   (id, first_name, last_name, date_of_birth)
 VALUES
   (159, 'Tjabbe', 'Tjibsma', '04-04-2004');
+
+-- An enabled user (pw: 'wimzusjet')
+INSERT INTO auth."user"
+  (id, email_address, password, role, enabled, public)
+VALUES
+  (357, 'user5@example.com', 'wimzusjet', 'divelog_member', false, true);
+INSERT INTO public.diver
+  (id, first_name, last_name, date_of_birth)
+VALUES
+  (357, 'Ed', 'Hooydonck, van', '05-05-2005');
+
+
+-- Dives for user Henk (authenticated user)
+INSERT INTO public."dive"
+  (diver_id, dive_site_id, dive_nr, depth, date, safety_stop, bottom_time, time_in, time_out, tank_pressure_start, tank_pressure_end, cylinder, water_type)
+VALUES
+  (123, 1, 10, 15, '01-01-2010', TRUE, INTERVAL 'PT45M', '13:00 UTC', '14:00 UTC', 200, 60, (12, 'steel'), 'salt'),
+  (123, 1, 11, 15, '02-01-2010', TRUE, INTERVAL 'PT45M', '13:00 UTC', '14:00 UTC', 200, 60, (12, 'steel'), 'salt'),
+  (123, 1, 12, 15, '03-01-2010', TRUE, INTERVAL 'PT45M', '13:00 UTC', '14:00 UTC', 200, 60, (12, 'steel'), 'salt');
+
+-- Dives for user Tsjabbe (non-public user, enabled)
+INSERT INTO public."dive"
+  (diver_id, dive_site_id, dive_nr, depth, date, safety_stop, bottom_time, time_in, time_out, tank_pressure_start, tank_pressure_end, cylinder, water_type)
+VALUES
+  (159, 1, 21, 15, '01-02-2020', TRUE, INTERVAL 'PT45M', '13:00 UTC', '14:00 UTC', 200, 60, (12, 'steel'), 'salt'),
+  (159, 1, 22, 15, '02-02-2020', TRUE, INTERVAL 'PT45M', '13:00 UTC', '14:00 UTC', 200, 60, (12, 'steel'), 'salt'),
+  (159, 1, 23, 15, '03-02-2020', TRUE, INTERVAL 'PT45M', '13:00 UTC', '14:00 UTC', 200, 60, (12, 'steel'), 'salt');
+
+-- Dives for user Koos (public user, enabled)
+INSERT INTO public."dive"
+  (diver_id, dive_site_id, dive_nr, depth, date, safety_stop, bottom_time, time_in, time_out, tank_pressure_start, tank_pressure_end, cylinder, water_type)
+VALUES
+  (789, 1, 30, 15, '01-03-2030', TRUE, INTERVAL 'PT45M', '13:00 UTC', '14:00 UTC', 200, 60, (12, 'steel'), 'salt'),
+  (789, 1, 31, 15, '02-03-2030', TRUE, INTERVAL 'PT45M', '13:00 UTC', '14:00 UTC', 200, 60, (12, 'steel'), 'salt'),
+  (789, 1, 32, 15, '03-03-2030', TRUE, INTERVAL 'PT45M', '13:00 UTC', '14:00 UTC', 200, 60, (12, 'steel'), 'salt');
+
+-- Dives for user Ed (public user, disabled)
+INSERT INTO public."dive"
+  (diver_id, dive_site_id, dive_nr, depth, date, safety_stop, bottom_time, time_in, time_out, tank_pressure_start, tank_pressure_end, cylinder, water_type)
+VALUES
+  (357, 1, 40, 15, '01-04-2040', TRUE, INTERVAL 'PT45M', '13:00 UTC', '14:00 UTC', 200, 60, (12, 'steel'), 'salt'),
+  (357, 1, 41, 15, '02-04-2040', TRUE, INTERVAL 'PT45M', '13:00 UTC', '14:00 UTC', 200, 60, (12, 'steel'), 'salt'),
+  (357, 1, 42, 15, '03-04-2040', TRUE, INTERVAL 'PT45M', '13:00 UTC', '14:00 UTC', 200, 60, (12, 'steel'), 'salt');
