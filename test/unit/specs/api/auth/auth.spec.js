1b3a08d706d00c8909cd750e29ff269844b2eaa6
Rocco Bruyn <rocco.bruyn@gmail.com>
2017-05-01 12:43:56 +0200

    Add tests for Auth signup method

--- test/unit/specs/api/auth/auth.spec.js
+++ test/unit/specs/api/auth/auth.spec.js
@@ -60,4 +60,28 @@ describe('Authentication', () => {
 
     return p;
   });
+
+  /**
+   * @test  {Auth#constructor}
+   */
+  it('should throw on invalid arguments', () => {
+    const auth         = new Auth({}),
+          emailAddress = 'user@example.com',
+          password     = 'foobarbazqux',
+          firstName    = 'Fred',
+          lastName     = 'Spekvet',
+          dateOfBirth  = new Date(),
+          futureDate   = new Date();
+
+    futureDate.setDate(dateOfBirth.getDate() + 1);
+
+    expect(() => { auth.signup(0, password, firstName, lastName, dateOfBirth); }).to.throw(TypeError);
+    expect(() => { auth.signup('notemail', password, firstName, lastName, dateOfBirth); }).to.throw(RangeError);
+    expect(() => { auth.signup(emailAddress, 0, firstName, lastName, dateOfBirth); }).to.throw(TypeError);
+    expect(() => { auth.signup(emailAddress, 'foo', firstName, lastName, dateOfBirth); }).to.throw(Error);
+    expect(() => { auth.signup(emailAddress, password, 0, lastName, dateOfBirth); }).to.throw(TypeError);
+    expect(() => { auth.signup(emailAddress, password, firstName, 0, dateOfBirth); }).to.throw(TypeError);
+    expect(() => { auth.signup(emailAddress, password, firstName, lastName, 0); }).to.throw(TypeError);
+    expect(() => { auth.signup(emailAddress, password, firstName, lastName, futureDate); }).to.throw(RangeError);
+  });
 });
