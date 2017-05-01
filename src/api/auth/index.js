af5f879bcaa4772c3b875ea232a2419fb7a78571
Rocco Bruyn <rocco.bruyn@gmail.com>
2017-05-01 11:59:13 +0200

    Add test for Auth signup method

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -61,4 +61,52 @@ export default class Auth {
         return success;
       });
   }
+
+  /**
+   *
+   * @param {String}  emailAddress
+   * @param {String}  password
+   * @param {String}  firstName
+   * @param {String}  lastName
+   * @param {Date}    dateOfBirth
+   */
+  signup(emailAddress, password, firstName, lastName, dateOfBirth) {
+    if (!isString(emailAddress)) {
+      throw new TypeError('Expected e-mail address to be string');
+    } else if (!isEmail(emailAddress)) {
+      throw new RangeError('E-mail address appears to be invalid');
+    }
+
+    if (!isString(password)) {
+      throw new TypeError('Expected password to be string');
+    } else if (8 > password.length) {
+      throw new Error('Password should be at least 8 characters long');
+    }
+
+    if (!isString(firstName)) {
+      throw new TypeError('Expected first name to be string');
+    }
+
+    if (!isString(lastName)) {
+      throw new TypeError('Expected last name to be string');
+    }
+
+    if (!isDate(dateOfBirth)) {
+      throw new TypeError('Expected date of birth to be date');
+    } else if (new Date() < dateOfBirth) {
+      throw new RangeError('Date of birth cannot be in the future');
+    }
+
+    return ns(this).restClient
+      .rpc('signup', {
+        email_address: emailAddress,
+        password     : password,
+        first_name   : firstName,
+        last_name    : lastName,
+        date_of_birth: dateOfBirth
+      })
+      .then(result => {
+        console.log('SIGNUP', result);
+      });
+  }
 }
--- test/unit/specs/api/auth/auth.spec.js
+++ test/unit/specs/api/auth/auth.spec.js
@@ -29,4 +29,35 @@ describe('Authentication', () => {
 
     return p;
   });
+
+  /**
+   * @test  {Auth#signup}
+   */
+  it('should call the signup RPC method', () => {
+    const rest         = {rpc: () => {}},
+          mock         = sinon.mock(rest),
+          auth         = new Auth(rest),
+          emailAddress = 'foo@example.com',
+          password     = 'foobarbazqux',
+          firstName    = 'Fred',
+          lastName     = 'Spekvet',
+          dateOfBirth  = new Date();
+
+    mock
+      .expects('rpc')
+      .withArgs('signup', {
+        email_address: emailAddress,
+        password     : password,
+        first_name   : firstName,
+        last_name    : lastName,
+        date_of_birth: dateOfBirth
+      })
+      .returns(Promise.resolve(true));
+
+    const p = auth.signup(emailAddress, password, firstName, lastName, dateOfBirth);
+
+    mock.verify();
+
+    return p;
+  });
 });
